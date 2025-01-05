44c0d6cd23becbb12535d631dbdbc12ba148327f
nkdas7582 <fragrancenishantad24@gmail.com>
2025-01-05 11:16:03 +0530

    2nd commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express =require("express");
 const app= express();
 const mongoose=require("mongoose");
 const Listing =require("./Models/listing.js");
+const methodOverride =require("method-override");
 const Mongo_URL="mongodb://127.0.0.1:27017/wanderlust";
 const path =require("path");
 main().then(()=>{
@@ -18,7 +19,7 @@ async function main() {
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
-
+app.use(methodOverride("_method"));
 app.get("/",(req,res)=>{
     res.send("working");
 });
@@ -28,13 +29,48 @@ app.get("/listings", async(req,res)=>{
         console.log(allListings);
         res.render("listings/index.ejs",{allListings});
     });
-    app.get("listings/:id"),async(req,res)=>{
+
+  //new route
+  app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+})
+//show route
+ app.get("/listings/:id",async(req,res)=>{
         let{id}=req.params;
        const listing=await Listing.findById(id);
-       res.render("show.ejs",{listing});
+       res.render("listings/show.ejs",{listing});
       
-    };
-    
+    });
+    //create route
+    app.post("/listings",async(req,res)=>{
+       // let{title,description,image,price,country}=req.body;
+   // let listing=await req.body.listing;
+ let newListing= new Listing (req.body.listing);
+newListing.save();
+res.redirect("/listings");
+    });
+    //Edit route
+app.get("/listings/:id/edit",async(req,res)=>{
+    let{id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+
+  //update route
+app.put("/listings/:id",async(req,res)=>{
+    let{id}=req.params; //extract id
+   await Listing.findByIdAndUpdate(id,{...req.body.listing});
+   res.redirect("/listings");
+
+});
+
+
+
+
+
+
+
+
 // app.get("/testListing",async (req,res)=>{
 //    let samplelisting = new Listing({
 // title:"my New villa",
