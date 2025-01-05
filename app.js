c6bb64101ac936007e290607d7f824136ac3abec
nkdas7582 <fragrancenishantad24@gmail.com>
2025-01-06 01:53:46 +0530

    3rd commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose=require("mongoose");
 const Listing =require("./Models/listing.js");
 const methodOverride =require("method-override");
 const Mongo_URL="mongodb://127.0.0.1:27017/wanderlust";
+const ejsMate=require("ejs-mate");
 const path =require("path");
 main().then(()=>{
     console.log("connectd to DB")
@@ -20,6 +21,8 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname,"/public")));
+app.engine("ejs", ejsMate);
 app.get("/",(req,res)=>{
     res.send("working");
 });
@@ -64,7 +67,13 @@ app.put("/listings/:id",async(req,res)=>{
 
 });
 
-
+//delete route
+app.delete("/listings/:id",async(req,res)=>{
+    let{id}=req.params; //extract id
+    let deletedListing=await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
 
 
 
